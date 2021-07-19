1603f1c99c9013823146bb5ee011179b08ac9d2e
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-07-19 16:58:20 -0400

    server updated

--- server.js
+++ server.js
@@ -16,8 +16,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+
 });
 
 // routes
